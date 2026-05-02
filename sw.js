ec7197eacefd9dd2d602ee2e046dcd51993e0d2c
mollylmangan <35754448+mollylmangan@users.noreply.github.com>
2026-05-01 20:47:59 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'motorway-v1';
+const SHELL = ['/', '/index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+));
